49642d136f4d15e73040bf60c21115b598a997d0
Matthew Harrison <Matthew.Harrison@noaa.gov>
2021-12-09 10:41:02 -0500

    initial commit of PIG2km examples

---
+++ ocean_only/PIG2km_taub_m1_10LatLon/common/INPUT/wget.bash
@@ -0,0 +1,3 @@
+wget ftp://ftp.gfdl.noaa.gov/pub/Matthew.Harrison/MOM6-IceShelf-examples/ocean_only/PIG2km_taub_m1_10LatLon/PIG2kmp1_bbar_taub_m1_10nxny_bedelev_outcrp.nc
+wget ftp://ftp.gfdl.noaa.gov/pub/Matthew.Harrison/MOM6-IceShelf-examples/ocean_only/PIG2km_taub_m1_10LatLon/PIG2km_supergrid.nc
+wget ftp://ftp.gfdl.noaa.gov/pub/Matthew.Harrison/MOM6-IceShelf-examples/ocean_only/PIG2km_taub_m1_10LatLon/sfc_ic.nc
